7000404dd856a6c87be048f132e85c87d17c7efb
Pavel Karpuk <pavel.karpuk.96@mail.ru>
2022-03-09 21:11:10 +0300

    add Fibonacci Numbers

---
+++ src/main/java/FibonacciNumbers.java
@@ -0,0 +1,24 @@
+public class FibonacciNumbers {
+
+    public static void main(String[] args) {
+        StringBuilder numbers = new StringBuilder();
+        int count = 5;
+        for (int i = 1; i <= count; i++) {
+            numbers.append(fibonacci(i)).append(", ");
+        }
+        numbers.deleteCharAt(numbers.length() - 2);
+        System.out.print(numbers);
+    }
+
+    private static int fibonacci(int index) {
+        if (index <= 0) {
+            return 0;
+        } else if (index == 1) {
+            return 1;
+        } else if (index == 2) {
+            return 1;
+        } else {
+            return fibonacci(index - 1) + fibonacci(index - 2);
+        }
+    }
+}
